0eea1b813c5a5cf8809ed4645213a5d2ecb4f469
Sonny Falk <sonnyfalk@gmail.com>
2025-03-07 11:53:19 -0800

    Add simple class definition tag for indexing.

--- queries/tags.scm
+++ queries/tags.scm
@@ -1,3 +1,7 @@
 (use_statement
   (file_name) @name
   (#set! index.element file_dependency)) @reference.file_dependency
+
+(class_definition
+  (class_header
+    name: (identifier) @name) @reference.class_definition (#set! index.element class_definition))
