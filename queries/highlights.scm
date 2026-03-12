83a153316a482a48bd16bee6da224f21bca604bb
Sonny Falk <sonnyfalk@gmail.com>
2026-03-12 14:23:37 -0700

    Add syntax highlighting for object reference.

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -9,11 +9,15 @@
 (number_literal) @number
 
 (object_header
-  name: (identifier) @entity.name.tag.object
+  name: (identifier) @entity.name.tag.object)
+
+(object_header
   superclass: (identifier) @entity.other.inherited-class)
 
 (class_header
-  name: (identifier) @entity.name.tag.class
+  name: (identifier) @entity.name.tag.class)
+
+(class_header
   superclass: (identifier) @entity.other.inherited-class)
 
 (function_header
@@ -25,8 +29,17 @@
 (send_statement
   name: (identifier) @entity.name.function.dataflex.send)
 
+(send_statement
+  receiver: (identifier) @entity.name.tag.object)
+
 (get_statement
   name: (identifier) @entity.name.function.dataflex.get)
 
+(get_statement
+  receiver: (identifier) @entity.name.tag.object)
+
 (set_statement
   name: (identifier) @entity.name.function.dataflex.set)
+
+(set_statement
+  receiver: (identifier) @entity.name.tag.object)
